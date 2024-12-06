f000190fcde936f689c635709fb32d9e2ddd1f17
exdisj <exdisj@outlook.com>
2024-12-06 13:53:18 -0500

    Added general module layout

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod core;
+pub mod calc;
+pub mod functions;
+pub mod io;
+pub mod commands;
